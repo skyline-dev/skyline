e7d6837c8327d9a58566dac7a7f24749a5c03192
Somebody Whoisbored <13044396+shadowninja108@users.noreply.github.com>
2019-12-31 22:38:01 -0700

    Fix linking with C++ standard library

--- include/alloc.h
+++ include/alloc.h
@@ -11,12 +11,12 @@
 extern "C" {
 #endif
 
-void*  WEAK malloc(size_t size);
-void  WEAK free(void *src);
-void*  WEAK calloc(u64 num, u64 size);
-void* WEAK  realloc(void *ptr, u64 size);
-void*  WEAK aligned_alloc(u64 alignment, u64 size);
-u64  WEAK malloc_usable_size(void *ptr);
+void*  malloc(size_t size);
+void free(void *src);
+void*  calloc(u64 num, u64 size);
+void*  realloc(void *ptr, u64 size);
+void*  aligned_alloc(u64 alignment, u64 size);
+u64  malloc_usable_size(void *ptr);
 
 #ifdef __cplusplus
 }
--- include/mem.h
+++ include/mem.h
@@ -11,11 +11,11 @@
 extern "C" {
 #endif
 
-void* WEAK memset(void *src, int val, u64 num);
-void* WEAK memcpy(void *dest, void const *src, u64 count);
-void* WEAK memmove( void* dest, const void* src, u64 count);
-void* WEAK memalign(size_t alignment, size_t size);
-void* WEAK memmem(void* needle, size_t needleLen, void* haystack, size_t haystackLen);
+void* memset(void *src, int val, u64 num);
+void* memcpy(void *dest, void const *src, u64 count);
+void* memmove( void* dest, const void* src, u64 count);
+void* memalign(size_t alignment, size_t size);
+void* memmem(void* needle, size_t needleLen, void* haystack, size_t haystackLen);
 
 #ifdef __cplusplus
 }
