379a84d7cf8597fd3c0c60a96ad69198ae8d8a2e
Somebody Whoisbored <13044396+shadowninja108@users.noreply.github.com>
2020-02-12 19:28:25 -0700

    Forgot this header

--- include/skyline/arc/Hashes.hpp
+++ include/skyline/arc/Hashes.hpp
@@ -2,27 +2,36 @@
 
 #include "types.h"
 #include "CRC32.h"
+#include "Arc.hpp"
 
-#include "../utils/cpputils.hpp"
+#include "skyline/logger/TcpLogger.hpp"
 
 #include <unordered_map>
 #include <string>
 #include <string.h>
+#include <iomanip>
+#include <sstream>
+#include <iterator>
+#include <set>
+
 
 namespace skyline {
 namespace arc {
     class Hashes {
         public:
 
-        std::unordered_map<uint, std::string> loadedHashes;
+        std::unordered_map<u32, std::string> loadedHashes;
 
         Hashes();
 
-        bool hasCrc(uint);
-        bool hasHash40(ulong);
+        bool hasCrc(u32);
+        bool hasHash40(skyline::arc::Arc::Hash40);
 
-        std::string lookupByCrc(uint);
-        std::string lookupByHash40(ulong);
+        std::string lookupByCrc(u32);
+        std::string lookupByHash40(skyline::arc::Arc::Hash40);
+
+        static skyline::arc::Arc::Hash40 create(std::string);
+        static skyline::arc::Arc::Hash40 createFromPath(std::string );
 
         ~Hashes();
     };
