e17a06e94e45214f7cb46b3b908b2a2d17533cff
Somebody Whoisbored <13044396+shadowninja108@users.noreply.github.com>
2020-02-14 00:14:53 -0700

    Forgot this

--- include/cxa.h
+++ include/cxa.h
@@ -14,6 +14,7 @@ extern "C" {
 extern s32 __cxa_guard_acquire(u32 *guard);
 extern void __cxa_guard_release(u32 *guard);
 extern void __cxa_pure_virtual();
+void __cxa_atexit();
 
 #ifdef __cplusplus
 }
