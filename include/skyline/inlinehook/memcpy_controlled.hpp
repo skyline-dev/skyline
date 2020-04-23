efc113016032b251adce5535726a092cbb11cf5c
jam1garner <jam1.mcleod@hotmail.com>
2020-04-23 16:10:54 -0400

    Add sky_memcpy for copying bytes into r-x or r-- memory

---
+++ include/skyline/inlinehook/memcpy_controlled.hpp
@@ -0,0 +1,3 @@
+#include "controlledpages.hpp"
+
+extern "C" Result sky_memcpy(void* dest, const void* src, size_t n);
---
+++ source/skyline/inlinehook/memcpy_controlled.cpp
@@ -0,0 +1,18 @@
+#include "skyline/inlinehook/memcpy_controlled.hpp"
+#include <string.h>
+
+extern "C" Result sky_memcpy(void* dest, const void* src, size_t n) {
+    if(dest == NULL || src == NULL) {
+        return -1;
+    }
+
+    skyline::inlinehook::ControlledPages control(dest, n);
+
+    control.claim();
+
+    memcpy(control.rw, src, n);
+
+    control.unclaim();
+
+    return 0;
+}
