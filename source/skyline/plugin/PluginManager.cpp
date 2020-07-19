39f4907a52e824a997f0281a1c3ab189f8ac72f9
3096 <contact@the3096.com>
2020-07-19 05:12:21 -0700

    make PluginManager instace & replace map with list

--- source/skyline/plugin/PluginManager.cpp
+++ source/skyline/plugin/PluginManager.cpp
@@ -2,38 +2,38 @@
 
 #include <set>
 
-namespace skyline {
-namespace plugin {
+#include "nn/crypto.h"
+#include "skyline/logger/TcpLogger.hpp"
 
-    void Manager::Init() {
+namespace skyline::plugin {
+
+    void Manager::LoadPluginsImpl() {
         Result rc;
 
         skyline::logger::s_Instance->LogFormat("[PluginManager] Initializing plugins...");
         
-        std::unordered_map<std::string, PluginInfo> plugins;
-        
         // walk through romfs:/skyline/plugins recursively to find any files and push them into map
-        skyline::utils::walkDirectory(utils::g_RomMountStr + "skyline/plugins", 
-        [&plugins](nn::fs::DirectoryEntry const& entry, std::shared_ptr<std::string> path) {
-            if(entry.type == nn::fs::DirectoryEntryType_File) // ignore directories
-                plugins[*path] = PluginInfo();
-        });
+        skyline::utils::walkDirectory(utils::g_RomMountStr + "skyline/plugins",
+                                      [this](nn::fs::DirectoryEntry const& entry, std::shared_ptr<std::string> path) {
+                                          if (entry.type == nn::fs::DirectoryEntryType_File)  // ignore directories
+                                              m_pluginInfos.push_back(PluginInfo{.Path = *path});
+                                      });
         
         skyline::logger::s_Instance->LogFormat("[PluginManager] Opening plugins...");
-        for (auto& kv : plugins) {
-            std::string path = kv.first;
-            PluginInfo& plugin = kv.second;
+        auto pluginInfoIter = m_pluginInfos.begin();
+        while (pluginInfoIter != m_pluginInfos.end()) {
+            auto& plugin = *pluginInfoIter;
 
             // open file
             nn::fs::FileHandle handle;
-            rc = nn::fs::OpenFile(&handle, path.c_str(), nn::fs::OpenMode_Read);
+            rc = nn::fs::OpenFile(&handle, plugin.Path.c_str(), nn::fs::OpenMode_Read);
             
             // file couldn't be opened, bail
-            if(R_FAILED(rc)){
-                skyline::logger::s_Instance->LogFormat("[PluginManager] Failed to open '%s' (0x%x). Skipping.", path.c_str(), rc);
-                
+            if (R_FAILED(rc)) {
+                skyline::logger::s_Instance->LogFormat("[PluginManager] Failed to open '%s' (0x%x). Skipping.",
+                                                       plugin.Path.c_str(), rc);
                 // stop tracking
-                plugins.erase(kv.first);
+                pluginInfoIter = m_pluginInfos.erase(pluginInfoIter);
                 continue;
             }
 
@@ -42,50 +42,62 @@ namespace plugin {
             nn::fs::CloseFile(handle); // file should be closed regardless of anything failing
             
             // getting file size failed, bail
-            if(R_FAILED(rc)) {
-                skyline::logger::s_Instance->LogFormat("[PluginManager] Failed to get '%s' size. (0x%x). Skipping.", path.c_str(), rc);
-                
+            if (R_FAILED(rc)) {
+                skyline::logger::s_Instance->LogFormat("[PluginManager] Failed to get '%s' size. (0x%x). Skipping.",
+                                                       plugin.Path.c_str(), rc);
+
                 // stop tracking
-                plugins.erase(kv.first);
-                
+                pluginInfoIter = m_pluginInfos.erase(pluginInfoIter);
                 continue;
             }
 
             plugin.Size = fileSize;
             plugin.Data = memalign(0x1000, plugin.Size);
 
-            rc = skyline::utils::readFile(path, 0, plugin.Data, plugin.Size);
-            if(R_SUCCEEDED(rc))
-                skyline::logger::s_Instance->LogFormat("[PluginManager] Read %s", path.c_str());
+            rc = skyline::utils::readFile(plugin.Path, 0, plugin.Data, plugin.Size);
+            if (R_SUCCEEDED(rc))
+                skyline::logger::s_Instance->LogFormat("[PluginManager] Read %s", plugin.Path.c_str());
             else {
-                skyline::logger::s_Instance->LogFormat("[PluginManager] Failed to read '%s'. (0x%x). Skipping.", path.c_str(), rc);
+                skyline::logger::s_Instance->LogFormat("[PluginManager] Failed to read '%s'. (0x%x). Skipping.",
+                                                       plugin.Path.c_str(), rc);
                 
                 // free space allocated for the file
                 free(plugin.Data);
                 
                 // stop tracking
-                plugins.erase(kv.first);
+                pluginInfoIter = m_pluginInfos.erase(pluginInfoIter);
+                continue;
             }
+            
+            pluginInfoIter++;
         }
 
         skyline::logger::s_Instance->LogFormat("[PluginManager] Calculating hashes...");
 
         // ro requires hashes to be sorted
         std::set<utils::Sha256Hash> sortedHashes;
-        for (auto& kv : plugins) {
-            PluginInfo& plugin = kv.second;
+        pluginInfoIter = m_pluginInfos.begin();
+        while (pluginInfoIter != m_pluginInfos.end()) {
+            auto& plugin = *pluginInfoIter;
             nn::ro::NroHeader* nro = (nn::ro::NroHeader*)plugin.Data;
             nn::crypto::GenerateSha256Hash(&plugin.Hash, sizeof(utils::Sha256Hash), nro, nro->size);
-            if (sortedHashes.find(plugin.Hash) == sortedHashes.end()) {
-                sortedHashes.insert(plugin.Hash);
-            } else {
-                // duplicate, stop tracking
-                plugins.erase(kv.first);
+
+            if (sortedHashes.find(plugin.Hash) != sortedHashes.end()) {
+                skyline::logger::s_Instance->LogFormat("[PluginManager] %s is detected duplicate, ignoring...",
+                                                       plugin.Path.c_str());
+
+                // stop tracking
+                pluginInfoIter = m_pluginInfos.erase(pluginInfoIter);
+                continue;
             }
+
+            sortedHashes.insert(plugin.Hash);
+            pluginInfoIter++;
         }
 
         // (sizeof(nrr header) + sizeof(sha256) * plugin count) aligned by 0x1000, as required by ro
-        size_t nrrSize = ALIGN_UP(sizeof(nn::ro::NrrHeader) + (plugins.size() * sizeof(utils::Sha256Hash)), 0x1000);
+        size_t nrrSize =
+            ALIGN_UP(sizeof(nn::ro::NrrHeader) + (m_pluginInfos.size() * sizeof(utils::Sha256Hash)), 0x1000);
 
         // get our own program ID
         // TODO: dedicated util for this
@@ -98,7 +110,7 @@ namespace plugin {
             .size = nrrSize,
             .type = 0, // ForSelf
             .hashes_offset = sizeof(nn::ro::NrrHeader),
-            .num_hashes = plugins.size(),
+            .num_hashes = m_pluginInfos.size(),
         };
         
         char* nrrBin = (char*) memalign(0x1000, nrrSize); // must be page aligned 
@@ -123,53 +135,66 @@ namespace plugin {
             free(nrrBin);
             
             // free all loaded plugins
-            for(auto& kv : plugins)
-                free(kv.second.Data);
+            // for(auto& kv : plugins)  TODO: refactor to smart pointer, no need to free
+            //     free(kv.second.Data);
             
             skyline::logger::s_Instance->LogFormat("[PluginManager] Failed to register NRR (0x%x).", rc);
             return;
         }
 
         skyline::logger::s_Instance->Log("[PluginManager] Loading plugins...\n");
-        for (auto& kv : plugins) {
-            PluginInfo& plugin = kv.second;
+        pluginInfoIter = m_pluginInfos.begin();
+        while (pluginInfoIter != m_pluginInfos.end()) {
+            auto& plugin = *pluginInfoIter;
 
             // attempt to get the required size for the bss 
             size_t bssSize;
-            if(R_FAILED(nn::ro::GetBufferSize(&bssSize, plugin.Data))) {
-                // ro rejected file, bail (the original input is not validated to be an actual NRO, so this isn't unusual)
+            rc = nn::ro::GetBufferSize(&bssSize, plugin.Data);
+            if (R_FAILED(rc)) {
+                // ro rejected file, bail
+                // (the original input is not validated to be an actual NRO, so this isn't unusual)
+                skyline::logger::s_Instance->LogFormat(
+                    "[PluginManager] nn::ro::GetBufferSize failed on %s (0x%x), not an nro?", plugin.Path.c_str(), rc);
                 
                 // we don't need the file data any more
                 free(plugin.Data);
                 
                 // stop tracking
-                plugins.erase(kv.first);
+                pluginInfoIter = m_pluginInfos.erase(pluginInfoIter);
                 
                 continue;
             }
 
             void* bss = memalign(0x1000, bssSize); // must be page aligned 
 
-            rc = nn::ro::LoadModule(&plugin.Module, plugin.Data, bss, bssSize, nn::ro::BindFlag_Now);// bind immediately, so all symbols are immediately available
+            rc = nn::ro::LoadModule(
+                &plugin.Module, plugin.Data, bss, bssSize,
+                nn::ro::BindFlag_Now);  // bind immediately, so all symbols are immediately available
  
-            if(R_SUCCEEDED(rc)) {
-                skyline::logger::s_Instance->LogFormat("[PluginManager] Loaded %s", kv.first.c_str(), &plugin.Module.Name);
+            if (R_SUCCEEDED(rc)) {
+                skyline::logger::s_Instance->LogFormat("[PluginManager] Loaded %s", plugin.Path.c_str(),
+                                                       &plugin.Module.Name);
             } else {
-               skyline::logger::s_Instance->LogFormat("[PluginManager] Failed to load %s, return code: 0x%x", kv.first.c_str(), rc);
-                
-               // couldn't be loaded, free unused memory
-               free(bss);
-               free(plugin.Data);
-                
+                skyline::logger::s_Instance->LogFormat("[PluginManager] Failed to load %s, return code: 0x%x",
+                                                       plugin.Path.c_str(), rc);
+
+                // couldn't be loaded, free unused memory
+                free(bss);
+                free(plugin.Data);
+
                 // stop tracking
-               plugins.erase(kv.first);
+                pluginInfoIter = m_pluginInfos.erase(pluginInfoIter);
+                continue;
             }
+
+            pluginInfoIter++;
         }
 
-        for (auto& kv : plugins) {
-            PluginInfo& plugin = kv.second;
+        pluginInfoIter = m_pluginInfos.begin();
+        while (pluginInfoIter != m_pluginInfos.end()) {
+            auto& plugin = *pluginInfoIter;
 
-            skyline::logger::s_Instance->LogFormat("[PluginManager] Running `main` for %s", kv.first.c_str(),
+            skyline::logger::s_Instance->LogFormat("[PluginManager] Running `main` for %s", plugin.Path.c_str(),
                                                    &plugin.Module.Name);
 
             // try to find entrypoint
@@ -179,15 +204,17 @@ namespace plugin {
                 &plugin.Module,
                 "main");
             
-            if(pluginEntrypoint != NULL && R_SUCCEEDED(rc)) {
+            if (pluginEntrypoint != NULL && R_SUCCEEDED(rc)) {
                 pluginEntrypoint();
                 skyline::logger::s_Instance->LogFormat("[PluginManager] Finished running `main` for %s, rc: 0x%x",
-                                                       kv.first.c_str(), rc);
+                                                       plugin.Path.c_str(), rc);
             } else {
                 skyline::logger::s_Instance->LogFormat(
-                    "[PluginManager] Failed to lookup symbol for %s, return code: 0x%x", kv.first.c_str(), rc);
+                    "[PluginManager] Failed to lookup symbol for %s, return code: 0x%x", plugin.Path.c_str(), rc);
             }
+            
+            pluginInfoIter++;
         }
     }
-};  // namespace plugin
-};  // namespace skyline
+
+};
