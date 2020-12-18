2995c4dc8e5b3a54543315a822140e577286374b
jam1garner <8260240+jam1garner@users.noreply.github.com>
2020-12-18 15:16:05 -0500

    Add simple NSO subsdk test

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,7 @@
 skyline::set_module_name!("skyline");
 
 fn main() {
-    use std::io::Write;
-    let mut test = std::net::TcpStream::connect(("192.168.0.1", 6969)).unwrap();
-
-    test.write(b"test\n").unwrap();
+    println!("Test");
 }
 
 #[no_mangle]
