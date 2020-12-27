79ff68d9d5c3d8d4929df14c8d6b0ebf555cb39f
shadowninja108 <13044396+shadowninja108@users.noreply.github.com>
2020-12-27 14:31:28 -0700

    Fix warning

--- src/cpp/include/cxa.h
+++ src/cpp/include/cxa.h
@@ -5,7 +5,7 @@
 extern "C" {
 #endif
 
-extern "C" void *__dso_handle = 0;
+extern "C" void *__dso_handle;
  
 extern "C" int __cxa_atexit(void (*destructor) (void *), void *arg, void *dso) {};
 extern "C" void __cxa_finalize(void *f) {};
